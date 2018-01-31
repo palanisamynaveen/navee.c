808dbf9e8db20d42ea48d499e64a129c9ce06f0a
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-01-31 15:59:14 +0530

    naveen

---
+++ naveen.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+clrscr();
+int a;
+printf("enter the number");
+scanf(%d",&a);
+if(a%2==0)
+{
+printf("the number %d is even");
+}
+else
+{
+printf("the number is %d odd");
+}
+getch();
+}
