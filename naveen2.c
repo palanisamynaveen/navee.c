e7a3121ac60ca8afb0313cbf917410ff4ff135ba
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-01-31 16:40:47 +0530

    naveen2.c

---
+++ naveen2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+inif((a>b)&&(a>c))
+
+{
+printf("a is greater");
+}
+else ((b>c)&&(b>a))
+{
+printf("b is greater);
+}
+else
+{
+printf("c is greater")
+}
+}
