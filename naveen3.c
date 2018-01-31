9e597d2d5e3af29075d1900730a0d6f6bcbcecf5
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-01-31 16:50:27 +0530

    naveen3.c

---
+++ naveen3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+scanf("d",&a);
+if (a%==0)
+{
+print("the year is leap year);
+}
+else
+{
+printf("the year ia not a leap year");
+}
+return 0;
+}
