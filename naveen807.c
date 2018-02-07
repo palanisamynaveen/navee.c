4807aabc2f4e6c1c00a5ae1b4704cae9fc34e682
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-02-07 16:00:37 +0530

    naveen807.c

---
+++ naveen807.c
@@ -0,0 +1,24 @@
+#include/,stdi0.h>
+#include<conio.h>
+int main()
+{
+int i,n,j,b,c;
+printf("enter the first number");
+scanf("%d",&n);
+printf("enter the last number");
+scanf("%d",&b);
+for(i=n;i<b;i++)
+if(i%j==0)
+{
+for(j=2;j<+i;i++)
+{
+c=1;
+break;
+}
+if(c==0)
+{
+printf("the prime number is &d",c);
+}
+}
+getch();
+}
