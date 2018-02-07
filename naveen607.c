b8b22a90d48b5d4ead8814bf177e0bca86400a8a
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-02-07 15:42:12 +0530

    naveen607.c

---
+++ naveen607.c
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+int num,m=20,n40;
+clrscr();
+printf("print value entered in m and c":\n");
+for(num=0;num<=0;num++)
+{
+if(num%2==1)
+printf("%d",num);
+}
+getch();
+}
