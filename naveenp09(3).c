a6063dbe8bdb834528cda620cdb2ba836c7dd0bd
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-02-09 00:16:14 -0800

    naveenp09(3).c

---
+++ naveenp09(3).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+    int num,i,n;
+    clrscr();
+    scanf("%d",&num);
+    for(i=1;i<=num;i++)
+    {
+        mul=i*5;
+        printf("%d\t",n);
+    }
+    getch();
+    return 0;
+}
