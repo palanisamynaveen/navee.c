e51e255c0f8d6eb5bf162f5782c45ceed07a545e
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-02-09 00:00:55 -0800

    naveenp09.c

---
+++ naveenp09.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,d,e,f;
+clrscr();
+printf("enter the number");
+scanf("%d",&a)
+b=a%100;
+c=b%10;
+d=c%10;
+e=d%10;
+f=(b*b*b)+(c*c*c)+(e*e*e);
+if(c==f)
+{
+printf("the number is armstrong number")
+}
+else
+{
+printf("the number is not a armstrong number ");
+}
+getch();
+return 0;
+}
