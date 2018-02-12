b6d420bdf5ceacf4fc0ea8e97c930b5370d5ab7a
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-02-12 15:54:10 +0530

    naveen121.c

---
+++ naveen121.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[10],temp;
+int i;
+clrscr();
+for(i=0;i<10;i++)
+{
+scanf(%d",%a[i];
+}
+temp=a[0];
+for(i=0;i<10;i++)
+{
+    if(a[i]>a[10])
+    {
+    temp=a[i];
+    }
+    printf("the smallest number is %d,a[i]);
+}
+getch();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
