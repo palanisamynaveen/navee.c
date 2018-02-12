914a50d8e67276f32d8e711030193bedd281efe6
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-02-12 16:12:32 +0530

    NAVEEN123.C

---
+++ NAVEEN123.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int array[10];
+int n,i,j,temp;
+clrscr();
+printf("\n enter the array size:");
+scanf("%d",&n);
+printf("\n enter the array elements:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&array[i]);
+}
+for (i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(array[i]>array[j])
+{
+temp=array[i];
+array[i]=array[j];
+array[j]=temp;
+}
+}
+}
+for(i=0;i<n;i++)
+{
+median=(array[n/2]+array[(n-1)/2])/2;
+}
+{
+printf("%d ",array[i]);
+}
+getch();
+return 0;
+}
