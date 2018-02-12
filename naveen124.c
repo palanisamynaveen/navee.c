ad4c3208a9fa5f9081d8c7d436ad904f0c5eee50
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-02-12 16:35:52 +0530

    naveen124.c

---
+++ naveen124.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int search(int arr[], int n, int x)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+if (arr[i] == x)
+return i;
+}
+return -1;
+} 
+int main()
+{
+scanf("%d", &n); 
+for (i=0; i<n; i++)
+scanf("%d",&arr[i]);
+scanf("%d", &x);
+printf("%d\n", search(arr, n, x));
+} 
+return 0;
+}
