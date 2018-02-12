fe226b05ccc5f3d4a36f4d8c5fcc4ef7592a5a97
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-02-12 15:45:09 +0530

    naveen12.c

---
+++ naveen12.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[10],temp;
+int i;
+for(i=0;i<10;i++)
+{
+scanf(%d",%a[i]_;
+}
+temp=a[0];
+for(i=0;i<10;i++)
+{
+    if(a[i]<a[10])
+    {
+    temp=a[i];
+    }
+    prinnf("the largest number is %d,tempa[i]);
+}
+getch();
+}
