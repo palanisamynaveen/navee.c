cb208b8aa8df4f4b65546b97a50d17bdc20e06f1
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-02-21 16:16:00 +0530

    naveennav.c

---
+++ naveennav.c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+char str[50];
+int i,count=0;
+clrscr();
+printf("enter the sentence:");
+scanf("%[^\n]s",str);
+for(i=0;str[i]!='\0';i++)
+{
+count=count+1;
+}
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]==' ')
+{
+count=count-1;
+}
+}
+printf("\nthe number of characters in the line are:%d",count);
+getch();
+return 0;
+}
