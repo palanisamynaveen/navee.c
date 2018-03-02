866345bfb29dd5a5c012d05424be9a7f5b33114a
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-03-02 13:33:16 +0530

    2naveen3.c

---
+++ 2naveen3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+int n;
+clrscr();
+printf("enter the number:");
+scanf("%d",&n);
+if(n>=1 && n<=10)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+return 0;
+}
