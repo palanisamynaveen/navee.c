4087eef91659c6f799af534c776a149f5d6ea8f6
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-03-05 16:11:08 +0530

    c.c

---
+++ c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+	int n1,n2,sum;
+  clrscr();
+	printf("\n enter the first number:");
+	scanf("%d",&n1);
+	printf("\n enter the second number:");
+	scanf("%d",&n2);
+	sum=n1+n2;
+	printf("%d",sum);
+  getch();
+	return 0;
+}
