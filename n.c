a52e9f51d5df703a75a2b5cb9aab0ae895f568b1
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-03-09 13:09:55 +0530

    n.c

---
+++ n.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+int x,y,temp;
+clrscr();
+printf("\n enter two numbers to swap:");
+scanf("%d %d",&x,&y);
+printf("\n before swapping:");
+printf("\nx=%d",x);
+printf("\ny=%d",y);
+temp=x;
+x=y;
+y=temp;
+printf("\n after swapping:");
+printf("\nx=%d",x);
+printf("\ny=%d",y);
+getch();
+return 0;
+}
