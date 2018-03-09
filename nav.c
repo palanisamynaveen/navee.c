893d10b89da85293919513955f487f537e05c4c5
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-03-09 13:06:24 +0530

    nav.c

---
+++ nav.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+int n,m,pro;
+clrscr();
+printf("enter the number1:");
+scanf("%d",&n);
+printf("\nenter the number2:");
+scanf("%d",&m);
+pro=n*m;
+if(pro%2==0)
+{
+printf("\n even");
+}
+else
+{
+printf("\n odd");
+}
+getch();
+return 0;
+ }
+ 
