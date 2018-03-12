c5fd6ecb6d7b805685c6ba58e6cd313b7ccf7ba1
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-03-12 15:47:48 +0530

    8.c

---
+++ 8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(d) 
+{
+	int num;
+  clrscr();
+	printf("enter the number:");
+	scanf("%d",&num);
+	num+=1;
+	while(num%10!=0)
+	{
+		num=num+1;
+	}
+	printf("\n%d",num);
+	return 0;
+}
