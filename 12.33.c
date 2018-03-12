72f970d14f9210011b91164359c018ba8fe28678
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-03-12 16:01:01 +0530

    12.33.c

---
+++ 12.33.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() 
+{
+	int n,l,r;
+  clrscr();
+	printf("\n enter the number:");
+	scanf("%d",&n);
+	printf("\n enter the left and right range:");
+	scanf("%d %d",&l,&r);
+	if(n>l && n<r)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+	return 0;
+}
