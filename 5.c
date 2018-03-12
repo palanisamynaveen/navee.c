f63272f55faa84ace728340e3dcb186cff6a2276
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-03-12 15:44:24 +0530

    5.c

---
+++ 5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+	char str[50];
+	int i;
+  clrscr();
+	printf("enter the string:");
+	scanf("%[^\n]s",str);
+	if(printf("\n%s",str))
+	{
+		
+	}
+	return 0;
+}
