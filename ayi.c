131a60115c48bf8282ca296b0929e898d4bdc8d6
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-03-14 15:46:34 +0530

    ayi.c

---
+++ ayi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+int n;
+clrscr();
+printf("enter the number:");
+scanf("%d",&n);
+if(n%13==0)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+return 0;
+}
