54ea5c0497d7706f8441efa7d9fa7eed13114da9
naveenpalanisamy <35765119+palanisamynaveen@users.noreply.github.com>
2018-03-19 16:12:13 +0600

    2006.c

---
+++ 2006.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+int x,y,remainder,gcd;
+clrscr();
+printf("\n enter the first number:");
+scanf("%d",&x);
+printf("\n enter the second number:");
+scanf("%d",&y);
+do
+{
+remainder=x%y;
+if(remainder==0)
+break;
+x=y;
+y=remainder;
+}while(remainder!=0);
+gcd=y;
+printf("\n the gcd of the given number is: %d",gcd);
+getch();
+return 0;
+}
