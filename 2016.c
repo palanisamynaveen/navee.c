18ace725576e22808dc93cacaa2954721faf24c2
naveenpalanisamy <35765119+palanisamynaveen@users.noreply.github.com>
2018-03-23 03:18:39 -0700

    2016.c

---
+++ 2016.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include>conio.h>
+int main() 
+ 
+{
+int n,remainder,reverse=0;
+ 
+int result;
+ 
+printf("\n enter the number:");
+ 
+scanf("%d",&n);
+ 
+while(n!=0)
+ 
+{
+ 
+remainder=n%10;
+ 
+reverse=reverse*10+remainder;
+ 
+n=n/10;
+ 
+}
+ 
+printf("\n%d",reverse);
+return 0;
+ 
+}
