cc74726847ee14dbc3036a779efdb206dc8a14b9
naveenpalanisamy <35765119+palanisamynaveen@users.noreply.github.com>
2018-03-28 15:41:51 +0530

    2026.c

---
+++ 2026.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+	int a,b,c,result;
+  clrscr();
+	scanf("%d %d %d",&a,&b,&c);
+	result=((a*b)/c);
+	printf("%d",result);
+  getch();
+	return 0;
+}
