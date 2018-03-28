f184326f25ac5a5a9ecec64a5bcbeec61d9a94ac
naveenpalanisamy <35765119+palanisamynaveen@users.noreply.github.com>
2018-03-28 16:21:21 +0530

    2029.c

---
+++ 2029.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,sum=0;
+clscr();
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("the sum is%d",sum);
+getch();
+return o;
+}
